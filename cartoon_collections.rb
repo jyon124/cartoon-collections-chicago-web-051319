f9d42ebdba91bfde1a1cdc3e45ef13803b35590a
Jooyon Won <jyon1204@gmail.com>
2019-05-05 19:31:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(dwarves)
   new_dwarves = []
   dwarves.each_with_index do |dwarves, number|
-    new_dwarves.push("#{number}. #{dwarves}")
+    new_dwarves << "#{number}. #{dwarves}"
   end
   return new_dwarves
 end
