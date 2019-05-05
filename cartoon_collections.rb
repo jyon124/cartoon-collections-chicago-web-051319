73cc528e62a09491cedca4136c134de49fc327c9
Jooyon Won <jyon1204@gmail.com>
2019-05-05 19:59:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def summon_captain_planet(veggies)
   veggies.each do |veggies|
     new_veggies << "#{veggies}#{exclamation}"
   end
-  puts new_veggies
+  puts summon_captain_planet(veggies)
 end
 
 def long_planeteer_calls# code an argument here
