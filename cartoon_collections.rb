d59817300a997c6df093dbd2dde070072e193e5e
Jooyon Won <jyon1204@gmail.com>
2019-05-05 19:50:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,10 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(veggies)
-  # Your code here
+  new_veggies = []
+  exclamation = "!"
+  veggies.each do |veggies|
+    new_veggies << "#{veggies}#{exclamation}".upperCase
 end
 
 def long_planeteer_calls# code an argument here
